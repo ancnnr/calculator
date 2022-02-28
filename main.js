875f61df232e824b9bf6aed55807f6ff9568bbf9
ancnnr <37272430+ancnnr@users.noreply.github.com>
2022-02-28 14:52:20 -0400

    Main structure setup
    Most css is done and working aside from minor tweaks. Needs JS functions to be written to create functionality

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+function clearScreen()
+{
+    //clears the screen
+    document.getElementById('screen').textContent = '';
+}
+
+function delChar() {
+    //delete right most character from screen
+}
+
+function numberPress()
+{
+    //place number on the screen and increment current values
+}
+
+function operatorPress() 
+{
+    //used for each operator press
+}
+
+function decimalPress()
+{
+    //used for decimal press
+}
+
+function equalPress()
+{
+    //used for when the equal sign is pressed
+}
