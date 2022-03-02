ece4e21eef8e763f910c9e369e03a44102c33217
ancnnr <ancnnr@gmail.com>
2022-03-02 08:36:23 -0400

    operation buttons all working now. Only thing left to add is decimal values

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let values = [];
+values[0]=0;
+values[1]=0;
+values[2]='';
+
 function loadFunction()
 {
     document.getElementById('9').addEventListener('click', function() {
@@ -30,18 +35,36 @@ function loadFunction()
     document.getElementById('0').addEventListener('click', function() {
         numberPress(0);
     });
+    document.getElementById('add').addEventListener('click', function() {
+        operatorPress('+');
+    });
+    document.getElementById('sub').addEventListener('click', function() {
+        operatorPress('-');
+    });
+    document.getElementById('mult').addEventListener('click', function() {
+        operatorPress('*');
+    });
+    document.getElementById('divide').addEventListener('click', function() {
+        operatorPress('÷');
+    });
+    document.getElementById('equals').addEventListener('click', function() {
+        equalPress();
+    });
 }
 
 
 function clearScreen()
 {
     //clears the screen
-    document.getElementById('screen').textContent = 0;
+    document.getElementById('val-screen').textContent = 0;
+    values[0] = 0;    
+    document.getElementById('calculation').textContent = ' ';
+    values[2] = '';
 }
 
 function delChar() {
     //delete right most character from screen
-    let curVal = parseInt(document.getElementById('screen').textContent);
+    let curVal = values[0];
     if(curVal>=10)
     {
         curVal = parseInt(curVal/10);
@@ -50,22 +73,43 @@ function delChar() {
     {
         curVal = 0;
     }
-    
-    document.getElementById('screen').textContent = curVal;
+
+    document.getElementById('val-screen').textContent = curVal;
+    values[0] = curVal;
 }
 
 function numberPress(x)
 {
     //place number on the screen and increment current values
-    console.log(x);
-    let curVal = parseInt(document.getElementById('screen').textContent);
+    let curVal = values[0];
     curVal = curVal*10 + parseInt(x);
-    document.getElementById('screen').textContent = curVal;
+    document.getElementById('val-screen').textContent = curVal;
+    values[0]=curVal;
 }
 
-function operatorPress() 
+function operatorPress(x) 
 {
     //used for each operator press
+    //darken button
+    //store values[0] in values array [1] (backup curVal)
+    //store operation in values[2] and clear screen
+    if(values[2]=='')
+    {
+        values[1] = values[0];
+        values[2] = x;
+        values[0] = 0;
+    }
+
+    else {
+        const res = getResult();
+        values[1] = res;
+        values[2] = x;
+        values[0] = 0;
+        document.getElementById('val-screen').textContent = res;
+    }
+    
+    document.getElementById('calculation').textContent= values[1] + ' ' + values[2] + ' ';
+
 }
 
 function decimalPress()
@@ -73,7 +117,41 @@ function decimalPress()
     //used for decimal press
 }
 
+function getResult()
+{
+    let result;
+
+    if(values[2]=='+')
+    {
+        result = values[1]+values[0];
+    }
+    else if(values[2]=='-')
+    {
+        result = values[1]-values[0]
+    }
+    else if(values[2]=='*')
+    {
+        result = values[1]*values[0];
+    }
+    else if(values[2]=='÷')
+    {   
+        if(values[0]===0)
+        {
+            result = 'ERR'
+        }
+        else {
+            result = values[1]/values[0];
+        }
+    }
+    return result;
+}
+
 function equalPress()
 {
     //used for when the equal sign is pressed
+    let result = getResult();
+
+    const upper = document.getElementById('calculation').textContent;
+    document.getElementById('calculation').textContent = upper + values[0] + ' = ';
+    document.getElementById('val-screen').textContent = result;    
 }
